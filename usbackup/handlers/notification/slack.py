2f80aa843459a2766d697799ead5474569c40ad5
Septi <septimiu.ujica@gmail.com>
2025-11-15 20:52:11 +0200

    slack file upload deprecation fix

--- usbackup/handlers/notification/slack.py
+++ usbackup/handlers/notification/slack.py
@@ -10,34 +10,58 @@ class SlackHandlerModel(HandlerBaseModel):
 
 class SlackHandler(NotificationHandler):
     handler: str = 'slack'
-    
+
     def __init__(self, model: SlackHandlerModel, *args, **kwargs) -> None:
         super().__init__(model, *args, **kwargs)
-        
+
         self._slack_token: str = model.token
         self._slack_channel: str = model.channel
-        
-        self._slack_api_url = 'https://slack.com/api/files.upload'
+
+        self._slack_get_url = "https://slack.com/api/files.getUploadURLExternal"
+        self._slack_complete_url = "https://slack.com/api/files.completeUploadExternal"
 
     async def notify(self, status: str, results: list[ResultModel], *, elapsed: datetime.timedelta) -> None:
         details = [res.message for res in results if res.message]
         details = "\n".join(details)
+        file = details.encode("utf-8")
+        filename = 'report.log'
 
         headers = {
-            'Authorization': f"Bearer {self._slack_token}",
+            "Authorization": f"Bearer {self._slack_token}",
+            "Content-Type": "application/json; charset=utf-8",
         }
 
-        params = {
-            'channels': self._slack_channel,
-            'filename': 'report.log',
-            'initial_comment': f'*{self._type.capitalize()} job "{self._name}" status: {status}*',
+        payload = {
+            "filename": filename,
+            "length": len(file),
         }
 
-        files = {
-            'file': details,
+        resp = await arequest_post(self._slack_get_url, headers=headers, params=payload)
+
+        if resp.status_code != 200 or not resp.json().get("ok"):
+            raise NotificationHandlerError(f"Slack exception: code: {resp.status_code}, response: {resp.text}", 1001)
+
+        resp_json = resp.json()
+        upload_url = resp_json["upload_url"]
+        file_id = resp_json["file_id"]
+
+        upload_resp = await arequest_post(upload_url, data=file)
+
+        if upload_resp.status_code != 200:
+            raise NotificationHandlerError(f"Slack upload exception: code: {upload_resp.status_code}, response: {upload_resp.text}", 1002)
+
+        payload = {
+            "files": [
+                {
+                    "id": file_id,
+                    "title": filename,
+                }
+            ],
+            "channels": self._slack_channel,
+            "initial_comment": f'*{self._type.capitalize()} job "{self._name}" status: {status} (Elapsed: {elapsed})*',
         }
 
-        resp = await arequest_post(self._slack_api_url, headers=headers, params=params, files=files)
+        send_resp = await arequest_post(self._slack_complete_url, headers=headers, json=payload)
 
-        if resp.status_code != 200 or not resp.json().get('ok'):
-            raise NotificationHandlerError(f'Slack exception: code: {resp.status_code}, response: {resp.text}', 1001)
+        if send_resp.status_code != 200 or not send_resp.json().get("ok"):
+            raise NotificationHandlerError(f"Slack exception: code: {send_resp.status_code}, response: {send_resp.text}", 1003)
